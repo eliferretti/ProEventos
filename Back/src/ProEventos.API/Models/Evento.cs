f5d65771e062fa1703ca9a7dad38f5d68ad5b9e1
Eli Ferretti <efilho@essence-rh.com.br>
2022-11-03 09:15:20 -0300

    first commit

--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -5,7 +5,7 @@
         public int EventoId { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
-        public string  Tema { get; set; }
+        public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
         public string ImagemUrl { get; set; }
--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -29,9 +29,14 @@ namespace ProEventos.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(
+                // SQLite
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+
+                // SQLServer
+                //context => context.UseSqlServer(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -53,6 +58,10 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors( x => x.AllowAnyHeader()
+                               .AllowAnyMethod()
+                               .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
